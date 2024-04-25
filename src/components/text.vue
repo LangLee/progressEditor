b0be937719021e977d12d5b23507f1da26c41863
李良 <liliang@yuanian.com>
2024-04-25 16:16:45 +0800

    vue3 vite tiptap

---
+++ src/components/text.vue
@@ -0,0 +1,24 @@
+<template>
+    <editor-content class="md-editor" :editor="editor"/>
+</template>
+<script setup>
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+import { ref, reactive } from 'vue'
+const editor = useEditor({
+    extensions: [
+        StarterKit,
+    ],
+    content: '<h1>Hello World!</h1>',
+})
+
+onBeforeUnmount(() => {
+  editor.destroy()
+})
+
+</script>
+
+
+<style lang='scss' scoped>
+
+</style>
